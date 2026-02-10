6b86e56855a1049d21c444096e1c6b5f0bb05177
brainshaker <parupp@gmx.de>
2026-02-10 21:55:08 +0100

    ci(release): release 0.0.1-alpha.2
    * test(eslint): normalize version of builtin plugin for snapshot
    
    * test(js): improve readability of exec calls
    
    * ci(release): fix packagist authentication
    
    * chore(release): bump version
    
    * ci(release): download artifacts to separate folder
    
    * ci(release): fix release workflow
    
    * ci(release): define missing env variables for input validation
    
    * ci(release): switch defaults of is-dry-run and do-ensure-set-secrets options
    
    * ci(release): fix access to workflow context in js scripts
    
    * ci(release): fix exec call to git rev-parse
    
    * ci(release): fix exec calls of @actions/exec
    
    * ci(release): add missing variable declaration

--- tests/js/eslint.test.ts
+++ tests/js/eslint.test.ts
@@ -6,6 +6,11 @@ import { traverseDirectory } from './utils/filesystem';
 
 test('expected eslint config', () => {
   traverseDirectory(`${process.cwd()}/tests/js/fixtures/eslint`, (filePath) => {
-    expect(execSync(`bun lint:js --print-config ${filePath}`, { encoding: 'utf8' }).replaceAll(process.cwd(), '.')).toMatchSnapshot();
+    const result = execSync(`bun lint:js --print-config ${filePath}`, { encoding: 'utf8' })
+      .replaceAll(process.cwd(), '.')
+      // NOTICE: Replace version of builtin plugin to avoid snapshot updates on every release
+      .replaceAll(/"brnshkr:brnshkr@.*",/gv, '"brnshkr:brnshkr@<version>",');
+
+    expect(result).toMatchSnapshot();
   });
 });
