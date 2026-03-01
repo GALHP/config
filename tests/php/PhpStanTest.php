da31789da593ed600cb787589cd1962dbe9d1baf
brainshaker <parupp@gmx.de>
2026-03-01 17:52:48 +0100

    test(php/phpstan): directly use new config class in 'PhpStanTest'

--- tests/php/PhpStanTest.php
+++ tests/php/PhpStanTest.php
@@ -5,15 +5,12 @@ declare(strict_types=1);
 namespace Brnshkr\Config\Tests;
 
 use Brnshkr\Config\Json;
+use Brnshkr\Config\PhpStanConfig;
 use JsonException;
-use PHPStan\DependencyInjection\ContainerFactory;
-use PHPStan\DependencyInjection\LoaderFactory;
-use PHPStan\File\FileHelper;
 use PHPUnit\Framework\Attributes\CoversNothing;
 use PHPUnit\Framework\TestCase;
 use Spatie\Snapshots\MatchesSnapshots;
 
-use function getcwd;
 use function is_array;
 
 /**
@@ -29,20 +26,7 @@ final class PhpStanTest extends TestCase
      */
     public function testExpectedPhpstanConfig(): void
     {
-        $currentWorkingDirectory = getcwd() ?: '.';
-        $containerFactory        = new ContainerFactory($currentWorkingDirectory);
-
-        // @phpstan-ignore-next-line phpstanApi.constructor, phpstanApi.method (Not covered by BC)
-        $loader = new LoaderFactory(
-            // @phpstan-ignore-next-line phpstanApi.constructor (Not covered by BC)
-            fileHelper: new FileHelper($currentWorkingDirectory),
-            rootDir: $containerFactory->getRootDirectory(),
-            currentWorkingDirectory: $containerFactory->getCurrentWorkingDirectory(),
-            generateBaselineFile: null,
-            expandRelativePaths: [],
-        )->createLoader();
-
-        $config = $loader->load($currentWorkingDirectory . '/conf/phpstan.php', null);
+        $config = PhpStanConfig::get();
 
         if (is_array($config['parameters'] ?? null)) {
             unset($config['parameters']['editorUrl']);
