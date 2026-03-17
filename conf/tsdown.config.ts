9f830fcd0417e62fcdf9685af34d01f69d0a2f88
brainshaker <parupp@gmx.de>
2026-03-17 19:55:16 +0100

    build(updates): update tsdown configuration after version update

--- conf/tsdown.config.ts
+++ conf/tsdown.config.ts
@@ -4,6 +4,7 @@ export default defineConfig((options) => {
   const isWatchMode = options.watch === true;
 
   const commonOptions = <const>{
+    outDir: '../dist',
     format: 'esm',
     treeshake: !isWatchMode,
     clean: !isWatchMode,
@@ -15,19 +16,24 @@ export default defineConfig((options) => {
   return [
     {
       ...commonOptions,
-      entry: [
-        'src/js/eslint/index.ts',
-        'src/js/stylelint/index.ts',
-      ],
       dts: !isWatchMode,
+      entry: [
+        '../src/js/eslint/index.ts',
+        '../src/js/stylelint/index.ts',
+      ],
+      deps: {
+        neverBundle: [
+          '@typescript-eslint/utils',
+        ],
+      },
     },
     {
       ...commonOptions,
+      outDir: `${commonOptions.outDir}/scripts`,
       entry: [
-        'scripts/eslint.ts',
-        'scripts/stylelint.ts',
+        '../scripts/eslint.ts',
+        '../scripts/stylelint.ts',
       ],
-      outDir: 'dist/scripts',
     },
   ];
 });
