ddad4e97d4d94829ff3e0a8958b0c83d8cb1d298
brainshaker <parupp@gmx.de>
2026-04-14 21:21:43 +0200

    feat(php/finder)!: add support for different file extensions
    BREAKING CHANGE: Removed PhpFileFinder::configure

--- src/php/PhpFileFinder.php
+++ src/php/PhpFileFinder.php
@@ -4,12 +4,18 @@ declare(strict_types=1);
 
 namespace Brnshkr\Config;
 
+use InvalidArgumentException;
 use LogicException;
 use Symfony\Component\Finder\Exception\DirectoryNotFoundException;
 use Symfony\Component\Finder\Finder;
 
 use function array_keys;
+use function array_map;
+use function implode;
+use function in_array;
+use function is_array;
 use function iterator_to_array;
+use function sprintf;
 
 /**
  * @api
@@ -18,48 +24,51 @@ use function iterator_to_array;
  */
 final class PhpFileFinder
 {
+    public const string EXTENSION_PHP  = 'php';
+    public const string EXTENSION_TWIG = 'twig';
+
+    /**
+     *  @phpstan-var list<self::EXTENSION_*>
+     */
+    public const array EXTENSIONS = [
+        self::EXTENSION_PHP,
+        self::EXTENSION_TWIG,
+    ];
+
     private function __construct() {}
 
     /**
+     * @param self::EXTENSION_*|list<self::EXTENSION_*> $extensions
+     *
      * @throws DirectoryNotFoundException
+     * @throws InvalidArgumentException
      */
-    public static function get(?Finder $finder = null): Finder
+    public static function get(?Finder $finder = null, string|array $extensions = self::EXTENSION_PHP): Finder
     {
-        if (!$finder instanceof Finder) {
+        if ($finder instanceof Finder) {
+            try {
+                $finder->getIterator();
+            } catch (LogicException) {
+                $finder->in('.');
+            }
+        } else {
             $finder = new Finder()->in('.');
         }
 
-        $finder = self::configure($finder);
+        $namePatterns = array_map(static function (string $extension): string {
+            if (!in_array($extension, self::EXTENSIONS, true)) {
+                throw new InvalidArgumentException(sprintf('Unsupported extension "%s". Supported extensions are: %s.', $extension, implode(', ', self::EXTENSIONS)));
+            }
 
-        try {
-            $finder->getIterator();
-        } catch (LogicException) {
-            $finder->in('.');
+            return '/\.' . $extension . '$/';
+        }, is_array($extensions) ? $extensions : [$extensions]);
+
+        if ($namePatterns !== []) {
+            $finder->name($namePatterns);
         }
 
-        return $finder;
-    }
-
-    /**
-     * @return list<string>
-     *
-     * @throws DirectoryNotFoundException
-     */
-    public static function getFilePaths(?Finder $finder = null): array
-    {
-        return self::get($finder)
-            |> iterator_to_array(...)
-            |> array_keys(...);
-    }
-
-    /**
-     * @internal
-     */
-    public static function configure(Finder $finder): Finder
-    {
         return $finder
             ->files()
-            ->name('/\.php$/')
             ->ignoreDotFiles(false)
             ->sortByCaseInsensitiveName(true)
             ->notPath([
@@ -75,5 +84,20 @@ final class PhpFileFinder
                 'vendor',
             ])
         ;
+    }
+
+    /**
+     * @param self::EXTENSION_*|list<self::EXTENSION_*> $extensions
+     *
+     * @return list<string>
+     *
+     * @throws DirectoryNotFoundException
+     * @throws InvalidArgumentException
+     */
+    public static function getFilePaths(?Finder $finder = null, string|array $extensions = self::EXTENSION_PHP): array
+    {
+        return self::get($finder, $extensions)
+            |> iterator_to_array(...)
+            |> array_keys(...);
     }
 }
